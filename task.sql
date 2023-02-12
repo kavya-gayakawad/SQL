49e274fd3ce49c27af641bc903714d94da261439
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2023-02-12 20:23:08 +0530

    Add files via upload

---
+++ task.sql
@@ -0,0 +1,9 @@
+
+/*CREATE DATABASE database_name;*/
+CREATE DATABASE aug_20
+use aug_20
+
+/*syntax for craeting table;*/
+CREATE TABLE table_name(column_name1 datatype,column_name2 datatype,column_name3 datatype);
+
+CREATE TABLE ipl_teams
